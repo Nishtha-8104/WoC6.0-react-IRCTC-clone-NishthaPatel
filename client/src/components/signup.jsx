e840756ce9a2cf5c8c51177138031599a5606173
Nishtha-8104 <143199132+Nishtha-8104@users.noreply.github.com>
2024-02-11 17:34:22 +0530

    Update signup.jsx

--- client/src/components/signup.jsx
+++ client/src/components/signup.jsx
@@ -8,59 +8,28 @@ function SignUp() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
+  const [isLoading, setLoading] = useState(false);
   const navigate = useNavigate();
 
-  // async function handlechange() {
-  //   if (username && password && email) {
-  //     try {
-  //       const statusOfSignUp = await fetch("https://irctc-crtv.onrender.com/signup", {
-  //         method: "POST",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //         },
-
-  //         body: JSON.stringify({ username, email, password }),
-  //       });
-
-  //       if (statusOfSignUp.ok) {
-  //         localStorage.removeItem("token");
-  //         localStorage.removeItem("username");
-
-  //         const response = await statusOfSignUp.json();
-  //         localStorage.setItem("token", response.token);
-  //         localStorage.setItem("username", response.username);
-
-  //         setRegistered(true);
-  //       } 
-  //     } catch (error) {
-  //       console.log("Error posting data", error);
-  //       alert("Please these username or password is taken, add other username or password");
-  //     }
-  //   } else {
-  //     alert("Please enter all required information");
-  //   }
-  // }
   async function handlechange() {
     if (username && password && email) {
+      setLoading(true); // Start loading
       try {
-        const statusOfSignUp = await fetch("https://irctc-woc.onrender.com/signup", {
+        const statusOfSignUp = await fetch("https://irctc-crtv.onrender.com/signup", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
-
           body: JSON.stringify({ username, email, password }),
         });
 
         if (statusOfSignUp.ok) {
           localStorage.removeItem("token");
           localStorage.removeItem("username");
-
           const response = await statusOfSignUp.json();
           localStorage.setItem("token", response.token);
           localStorage.setItem("username", response.username);
-          localStorage.setItem("email", email);
-
+          console.log(response.token);
           setRegistered(true);
         } else {
           setFalseuser("Username is taken!😕");
@@ -68,6 +37,8 @@ function SignUp() {
       } catch (error) {
         console.log("Error posting data", error);
         alert("Error signing up");
+      } finally {
+        setLoading(false); // Stop loading
       }
     } else {
       alert("Please enter all required information");
@@ -83,42 +54,42 @@ function SignUp() {
   }
 
   return (
-      <Card className="container signup">
+    <Card className="container signup">
       <div className="main">
-      <form className="form">
-        <h1>SignUp</h1>
-        <br></br>
-        <label>Email: </label>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <br />
-        <label>Username: </label>
-        <input
-        className="input"
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <br />
-        <label>Password: </label>
-        <input
-          type="password"
-          className="input"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <br />
-        <Button onClick={handlechange}>Sign up</Button>
-
-        <br />
-        <br />
+        <form className="form">
+          <h1>SignUp</h1>
+          <br></br>
+          <label>Email: </label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br />
+          <label>Username: </label>
+          <input
+            className="input"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <br />
+          <label>Password: </label>
+          <input
+            type="password"
+            className="input"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <Button onClick={handlechange}>
+            {isLoading ? "Loading..." : "Sign up"}
+          </Button>
+          <br />
+          <br />
         </form>
-        </div>
-      </Card>
-    
+      </div>
+    </Card>
   );
 }
 
